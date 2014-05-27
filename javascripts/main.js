9a6d476258f334d0c331d6f2aee5c753ff791578
Albert Vilà Picas <albertvilapicas@gmail.com>
2014-05-27 21:48:39 +0200

    first template for example

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,30 @@
-console.log('This would be the main JS file.');
+
+Carousel = Backbone.View.extend({
+  events: {
+    'click .carousel-prev': 'prev',
+    'click .carousel-next': 'next'
+  },
+  initialize: function(options) {
+    _.bindAll(this);
+    this.items = _.map(this.$('.carousel-item').hide(), function(i) { return i; });
+    this.current = 0;
+  },
+  render: function() {
+    $(this.items[this.current]).show();
+    return this;
+  },
+  prev: function() {
+    $(this.items[this.current]).fadeOut(function() {
+      this.current = this.current - 1;
+      if (this.current === -1) { this.current = this.items.length - 1 }
+      $(this.items[this.current]).fadeIn();
+    }.bind(this));
+  },
+  next: function() {
+    $(this.items[this.current]).fadeOut(function() {
+      this.current = this.current + 1;
+      if (this.current === this.items.length) { this.current = 0 }
+      $(this.items[this.current]).fadeIn();
+    }.bind(this));
+  }
+});

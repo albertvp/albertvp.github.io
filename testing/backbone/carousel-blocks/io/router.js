06535dfe581e64493e9da8d193e7bb76f521d2f9
Albert Vilà Picas <albertvilapicas@gmail.com>
2014-06-03 22:01:16 +0200

    added submodule

--- testing/backbone/carousel-blocks/io/router.js
+++
@@ -1,9 +0,0 @@
-window.Router = Backbone.Router.extend({
-	routes: {
-		'': 'index'
-	},
-	index: function(){
-		if (!app.views.blocks) app.views.blocks = new Blocks();
-		//app.views.carousel = new Carousel({el: '#carousel'}).render();
-	}
-});
